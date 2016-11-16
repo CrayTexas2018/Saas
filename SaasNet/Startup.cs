d9d06b9af407c0c03e00e09f05e3fbf2cd2993d7
cray <cray.jaeger1@gmail.com>
2016-11-16 03:51:33 +0000

    changes to use identity and mobile friendly

---
+++ SaasNet/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SaasNet.Startup))]
+namespace SaasNet
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
